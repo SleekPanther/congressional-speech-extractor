67ea37f7e3d3cac3d97f39c4672a49df799be5f8
SleekPanther <macrobyte2@gmail.com>
2018-10-07 17:00:51 -0400

    Detect speech starts & clerk interjections

--- CongressionalSpeechExtractor.java
+++ CongressionalSpeechExtractor.java
@@ -27,36 +27,61 @@ public class CongressionalSpeechExtractor {
 				StringBuilder speech = new StringBuilder();
 				boolean speechStarted=false;
 				
-				String prevousLine = reader.readLine();
-				String line = "";	//assume 1st 2 lines are skippable & 2 lines exist
-				int j=0;
-				for(; (line = reader.readLine()) != null && j<4; j++){
+				String previousLine = reader.readLine();
+				String line = reader.readLine();	//assume 1st 2 lines are skippable & 2 lines exist
+				int j=2;		//line numbers start from 1 & previousLine uses up line 1
+				for(; line != null && j<958; previousLine=line, line=reader.readLine(), j++){
 					boolean isPreviousLineEndOfSentence  = false;
-					if(prevousLine.length()>0){
-						isPreviousLineEndOfSentence = prevousLine.substring(prevousLine.length()-1).matches("[\\.\\?!]");	//end of sentence = . ? ! 
+					if(previousLine.length()>0){
+						isPreviousLineEndOfSentence = previousLine.substring(previousLine.length()-1).matches("[\\.\\?!]");	//end of sentence = . ? ! 
 					}
-					// System.out.println(isPreviousLineEndOfSentence);
-					// System.out.println(line);
-					boolean titleDetected = line.matches("(?i:^((Mr\\.)|(Ms\\.)).*)");		//line starting with Mr. or Ms. with any text afterwards    (?i:X) is for case insensitive 
-//					System.out.println(titleDetected);
+					String[] words = line.split(" ");	//split line on spaces
+					boolean titleDetected = line.matches("(?i:^((Mr\\.)|(Ms\\.)).*)") && isUpperCase(words[1]) && isPreviousLineEndOfSentence;		//line starting with Mr. or Ms. with any text afterwards    (?i:X) is for case insensitive 
+					boolean clerkDetected = line.matches("(?i:^(The *CLERK).*)");
+					if(speechStarted && clerkDetected){	//speech ended, write to file
+						//speech.append(j+"=clerk\n\n");
+						speech.setLength(0);
+						speechStarted = false;
+					}
 					if(titleDetected){
-						String[] words = line.split(" ");
-						speakerName = words[0] + " " + words[1];
-						if(words[2].matches("(?i:of)")){
-							if(isState(words[3])){
-								speakerName += " of " + words[3];
-							}
-							else if(isState(words[3] + " " + words[4])){
-								speakerName += " of " + words[3] + " " + words[4];
-							}
-							//more cases if states names are > 2 words
+						if(speechStarted){	//end current speech & start new???
+							speech.append(line+"\n");
 						}
-						System.out.println(speakerName);
+						else{
+							speechStarted = true;
+							speakerName = extractSpeakerNameAndOptionalState(words);
+							speech.append(line+"\n");
+						}
 					}
-					// System.out.println("prev="+prevousLine+"\tline="+line);
-					prevousLine = line;
+					else if(speechStarted && !line.isEmpty()){
+						speech.append(line+"\n");
+					}
+					else if(speechStarted && line.isEmpty()){	//skip 3 column page breaks with dates
+						previousLine = line;	//update before skipping
+						line = reader.readLine();
+						j++;
+						if(line.split(" ")[0].matches("(?i:VerDate)")){
+							// speech.append("\tline="+j + " "+line+"\n");
+							for(; !line.split(" ")[0].matches("(?i:Jkt)"); previousLine=line, line=reader.readLine(), j++){
+								//Advance over page break date info
+								// speech.append("prev="+previousLine+"\tline="+line+"\n");
+							}
+							//Advance 2 more lines
+							previousLine=line;
+							line=reader.readLine();
+							j++;
+							previousLine=line;
+							line=reader.readLine();
+							j++;
+							// speech.append("prev="+previousLine+"\n");
+							// speech.append(j+" line="+line+"\n");
+						}
+
+					}
+
 					// System.out.println(line);
 				}
+				System.out.println(speech.toString());
 
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -88,6 +113,29 @@ public class CongressionalSpeechExtractor {
 		// }
 	}
 	
+	private static boolean isUpperCase(String string) {
+		return string.equals(string.toUpperCase());
+	}
+
+	//TODO = deal with periods after names?
+	private static String extractSpeakerNameAndOptionalState(String[] words) {
+		String speakerName = words[0] + " " + words[1];
+		if(words[2].matches("(?i:of)")){
+			if(isState(words[3])){
+				speakerName += " of " + words[3];
+			}
+			else if(isState(words[3] + " " + words[4])){
+				speakerName += " of " + words[3] + " " + words[4];
+			}
+			else{
+				speakerName += " NO STATE FOUND";
+			}
+			//more cases if states names are > 2 words
+		}
+		// System.out.println("Speaker="+speakerName);
+		return speakerName;
+	}
+
 	//states with more than 2 words?
 	private static ArrayList<String> states = new ArrayList<String>(Arrays.asList(new String[]{"alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming", "district of columbia", "puerto rico", "guam", "american samoa", "u.s. virgin islands", "northern mariana islands"}));
 	private static boolean isState(String state) {
